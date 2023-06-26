dd79fba1f6310eb15b98ffed6ea133e594cc6937
Mickey Camarco <48422179+mcamarco@users.noreply.github.com>
2023-06-26 14:35:24 -0400

    readme updated, script.js cleaned up

--- README.md
+++ README.md
@@ -1,23 +1,51 @@
-# The-19th-Hole
+# Weather App Readme
 
-## Description
+This is a weather application that allows users to retrieve weather information based on a specific zip code. The application displays the weather conditions, humidity, temperature, and wind speed for the given zip code. It also provides additional functionality to search for nearby golf courses and breweries based on the zip code.
 
-The purpose of this project was to work collaboratively in a group to build an app implementing two API'S, a new framework, and local storage.
+## Technologies Used
+
+- JavaScript
+- jQuery
+- HTML
+- CSS
+- Google Maps API
+- OpenWeatherMap API
+- Visual Crossing Weather API
+
+## Installation
+
+To use this application locally, follow these steps:
+
+1. Clone the repository to your local machine.
+2. Open the project folder.
+3. Open the `index.html` file in your preferred web browser.
 
 ## Usage
 
-To use this app you will need to access it in the browser. Type your local zip code in the search box. Five local golf courses, liquor stores, and the local weather will be displayed in the boxes on the wed page.
+1. Enter a zip code in the input field and click the submit button.
+2. The application will fetch the weather data for the provided zip code using the OpenWeatherMap and Visual Crossing Weather APIs.
+3. The weather information, including conditions, humidity, temperature, and wind speed, will be displayed on the page.
+4. The application will also retrieve nearby golf courses and breweries using the Google Maps API based on the provided zip code.
+5. The golf course and brewery information will be displayed on the page in separate sections.
+6. Previous zip codes that have been searched will be stored in the local storage and displayed as buttons under the "Local History" section.
+7. Clicking on a previous zip code button will fetch and display the weather and nearby locations for that zip code.
+
+## Contributing
+
+Contributions to this project are welcome. If you find any issues or have suggestions for improvement, please feel free to submit a pull request.
 
 ## Credits
 
-N/A
+- [Google Maps API](https://developers.google.com/maps/documentation)
+- [OpenWeatherMap API](https://openweathermap.org/api)
+- [Visual Crossing Weather API](https://www.visualcrossing.com/weather-data-editions)
 
 ## License
 
-MIT
+This project is licensed under the [MIT License](https://opensource.org/licenses/MIT).
 
 ## Deployed Link
 
-[https://mcamarco.github.io/the-19th-hole/]
+https://mcamarco.github.io/the-19th-hole/
 
 ![Screenshot 19th Hole](/assets/screenshot%20pro.png)
--- script/script.js
+++ script/script.js
@@ -1,3 +1,4 @@
+// Variable declarations
 var map;
 var service;
 var infowindow;
@@ -11,12 +12,13 @@ $("#formSubmit").on("submit", function (e) {
     getCoord(zipCode);
     console.log(zipCode);
 
-    // push zipcode to local storage
+    // Push zipcode to local storage
     storedData.unshift(zipCode);
     localStorage.setItem('myAppData', JSON.stringify(storedData));
     zipButtons();
 });
-// TODO: Get lat/lon from zipcode
+
+// Function to get latitude and longitude from zipcode
 function getCoord(zipCode) {
     fetch(`https://api.openweathermap.org/data/2.5/weather?zip=${zipCode}&limit=5&appid=${APIKeyWeather}`)
         .then(function (res) {
@@ -27,19 +29,12 @@ function getCoord(zipCode) {
             var lat = data.coord.lat;
             var lon = data.coord.lon;
             console.log(lat, lon);
-         
+
             initMap(lat, lon);
         });
 }
-            //  map information
-// function initMap(lat, lon) {
-//     var location = new google.maps.LatLng(lat, lon);
-//     // Use stored lat/lon, if available
-//     if (storedData.lat && storedData.lon) {
-//         initMap(storedData.lat, storedData.lon);
-//     }
-// }
-//  map information
+
+// Function to initialize the map
 function initMap(lat, lon) {
     var location = new google.maps.LatLng(lat, lon);
     map = new google.maps.Map(document.getElementById('map'), {
@@ -61,17 +56,12 @@ function initMap(lat, lon) {
     // Golf Courses - get data & append to HTML
     service.textSearch(request, function (results, status) {
         if (status === google.maps.places.PlacesServiceStatus.OK) {
-            //console.log(results)
             document.getElementById("golfcards").innerHTML = "";
-            // TODO: ADD FOR LOOP
             for (var i = 0; i < results.length - 15; i++) {
-                //console.log(results[i]);
                 var golfName = document.createElement("h2")
                 golfName.textContent = results[i].name;
                 var golfAddress = document.createElement("h2")
                 golfAddress.textContent = results[i].formatted_address;
-                // var golfImgURL = document.createElement("h2")
-                // golfImgURL.textContent = results[i].icon;
                 var golfcards = document.createElement("div");
                 golfcards.setAttribute("class", "golfcards");
                 golfcards.append(golfName, golfAddress);
@@ -79,20 +69,16 @@ function initMap(lat, lon) {
             }
         }
     });
+
     // Breweries - get data & append to HTML
     service.textSearch(request2, function (results, status) {
         if (status === google.maps.places.PlacesServiceStatus.OK) {
-            //console.log(results)
-            // TODO: ADD FOR LOOP - max 5
             document.getElementById("distributorsCards").innerHTML = "";
             for (var i = 0; i < results.length - 15; i++) {
-                //console.log(results[i]);
                 var distName = document.createElement("h2")
                 distName.textContent = results[i].name;
                 var distAddress = document.createElement("h2")
                 distAddress.textContent = results[i].formatted_address;
-                // var golfImgURL = document.createElement("h2")
-                // golfImgURL.textContent = results[i].icon;
                 var distcards = document.createElement("div");
                 distcards.setAttribute("class", "distributorsCards");
                 distcards.append(distName, distAddress);
@@ -102,8 +88,8 @@ function initMap(lat, lon) {
     })
 }
 
-// Store Data
-$(document).on("click", ".zip", function(e){
+// Event listener for zip code buttons
+$(document).on("click", ".zip", function (e) {
     e.preventDefault();
     var zip = $(this).attr("data-zip");
     console.log(zip);
@@ -111,55 +97,49 @@ $(document).on("click", ".zip", function(e){
     getCoord(zip);
 });
 
-// create zip code buttons under local history on HTML
-function zipButtons()
-{
+// Function to create zip code buttons under local history on HTML
+function zipButtons() {
     storedData = JSON.parse(localStorage.getItem('myAppData')) || [];
     $("#zip-buttons").text("");
 
-    if(storedData.length < 5)
-    {
-        for(var i=0; i<storedData.length;i++)
-    {
-        var btn = $("<button>");
-        btn.text(storedData[i]);
-        btn.attr("data-zip", storedData[i]);
-        btn.addClass("zip");
-        $("#zip-buttons").append(btn);
+    if (storedData.length < 5) {
+        for (var i = 0; i < storedData.length; i++) {
+            var btn = $("<button>");
+            btn.text(storedData[i]);
+            btn.attr("data-zip", storedData[i]);
+            btn.addClass("zip");
+            $("#zip-buttons").append(btn);
+        }
     }
+    else {
+        for (var i = 0; i < 5; i++) {
+            var btn = $("<button>");
+            btn.text(storedData[i]);
+            btn.attr("data-zip", storedData[i]);
+            btn.addClass("zip");
+            $("#zip-buttons").append(btn);
+        }
     }
-    else{
-        for(var i=0; i<5;i++)
-    {
-        var btn = $("<button>");
-        btn.text(storedData[i]);
-        btn.attr("data-zip", storedData[i]);
-        btn.addClass("zip");
-        $("#zip-buttons").append(btn);
-    }
-    }
-    
 }
 
 zipButtons();
 
-function getWeather(zipCode)
-{
+// Function to get weather data based on zip code
+function getWeather(zipCode) {
     fetch('https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/' + zipCode + '?key=BC56DV38KGW5TK327XDYHBPXD')
         .then(function (response) {
             return response.json()
         }).then(function (results) {
-            console.log(results)
-            // append weather
+            console.log(results);
             document.getElementById("weatherCards").innerHTML = "";
             var weatherStatus = document.createElement("h2");
-            weatherStatus.textContent="Conditions: "+ results.currentConditions.conditions;
+            weatherStatus.textContent = "Conditions: " + results.currentConditions.conditions;
             var weatherName = document.createElement("h2");
-            weatherName.textContent="Humidity: "+ results.currentConditions.humidity + " %";
+            weatherName.textContent = "Humidity: " + results.currentConditions.humidity + " %";
             var weatherTemp = document.createElement("h2");
-            weatherTemp.textContent="Feels like "+ results.currentConditions.feelslike +  " °F";
+            weatherTemp.textContent = "Feels like " + results.currentConditions.feelslike + " °F";
             var weatherWind = document.createElement("h2");
-            weatherWind.textContent="Wind speed: "+ results.currentConditions.windspeed + " mph";
+            weatherWind.textContent = "Wind speed: " + results.currentConditions.windspeed + " mph";
             var weatherCards = document.createElement("div");
             weatherCards.setAttribute("class", "weatherCards");
             weatherCards.append(weatherName);
@@ -170,16 +150,12 @@ function getWeather(zipCode)
         })
 }
 
-
-// adding variables to get the search. Save var needs to be updated
-const zipCode = document.querySelector('#zip')
-var submitForm = document.querySelector('#formSubmit')
-console.log(zipCode, submitForm);
-// gets the data from the API based off zip code
+// Event listener for submitting zip code form
+const zipCodeInput = document.querySelector('#zip');
+const submitForm = document.querySelector('#formSubmit');
 submitForm.addEventListener('submit', function (event) {
-    event.preventDefault()
-    var inputvalue = zipCode.value;
+    event.preventDefault();
+    var inputvalue = zipCodeInput.value;
     console.log(inputvalue);
     getWeather(inputvalue);
-})
-
+});
